7c31e42bd1cc1a9d7c16908b90d96d2d8ef98af9
Louis DeScioli <louis.descioli@gmail.com>
2015-01-28 05:21:46 -0500

    Name changes, autoprefixer, and less.json
    - You can now specify options for the package in config/less.json
    - Simplified the plugin name
    - Added the autoprefixer plugin

--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
-  name: "ordered-less",
+  name: "grove:less",
   summary: "Import all of your Less files in one step, in the order you want",
   version: "0.0.1"
 });
 
 Package.registerBuildPlugin({
-  name: 'compileLessIndex',
+  name: 'less-compiler',
   use: [],
   sources: [
     'plugin.js',
   ],
   npmDependencies: {
-    "less": "2.2.0"
+    "less": "2.2.0",
+    "less-plugin-autoprefix": "1.3.0"
   }
 });
